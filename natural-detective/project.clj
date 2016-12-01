6cf0559400603f9a306ee8ff7ddf31387ed917bc
Adei Josol <helena.josol@gmail.com>
2016-12-01 00:41:23 +0000

    Add proto-repl and tools.namespace dependencies

--- natural-detective/project.clj
+++ natural-detective/project.clj
@@ -3,8 +3,10 @@
             :url "https://github.com/adjl/NaturalDetective"
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [org.clojure/clojurescript "1.9.293"]
+                           [org.clojure/tools.namespace "0.2.11"]
                            [org.clojure/tools.nrepl "0.2.12"]
-                           [com.cemerick/piggieback "0.2.1"]]
+                           [com.cemerick/piggieback "0.2.1"]
+                           [proto-repl "0.3.1"]]
             :plugins [[lein-cljsbuild "1.1.4"]
                       [lein-figwheel "0.5.8"]]
             :clean-targets ^{:protect false} ["figwheel_server.log"
