a26fc17707ebb49120ebc787dae1ec1986b5c032
Adei Josol <helena.josol@gmail.com>
2016-12-02 21:56:37 +0000

    Eliminate unnecessary reified objects

--- natural-detective/src/game/core.cljs
+++ natural-detective/src/game/core.cljs
@@ -1,20 +1,27 @@
 (ns ^:figwheel-always game.core
-  (:require [cljsjs.phaser]
-            [game.boot :refer [Boot]]
-            [game.preload :refer [Preload]]
-            [game.title-screen :refer [TitleScreen]]
-            [game.main-game :refer [MainGame]]))
+  (:require [cljsjs.phaser]))
 
 (enable-console-print!)
 
 (defonce game (js/Phaser.Game.))
 
+(defn preload []
+  (let [scale (.-scale game)]
+    (set! (.-pageAlignHorizontally scale) true)
+    (set! (.-pageAlignVertically scale) true))
+  (.. game -load (image "logo" "phaser.png"))
+  (.. game -state (start "create")))
+
+(defn create []
+  (let [center-x (.. game -world -centerX)
+        center-y (.. game -world -centerY)
+        logo (.. game -add (sprite center-x center-y "logo"))]
+    (.. logo -anchor (setTo 0.5 0.5))))
+
 (defn -main []
   (let [state (.-state game)]
-    (.add state "boot" Boot)
-    (.add state "preload" Preload)
-    (.add state "titleScreen" TitleScreen)
-    (.add state "mainGame" MainGame)
-    (.start state "boot")))
+    (.add state "preload" (clj->js {:preload preload}))
+    (.add state "create" (clj->js {:create create}))
+    (.start state "preload")))
 
 (-main)
--- natural-detective/src/game/title_screen.cljs
+++
@@ -1,7 +0,0 @@
-(ns game.title-screen)
-
-(defn TitleScreen [game]
-  (let []
-    (reify Object
-      (create [this]
-        ()))))
