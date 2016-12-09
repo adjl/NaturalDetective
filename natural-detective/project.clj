e75769a4f0a13338f4c25f15372b1f4e83a460a8
Adei Josol <helena.josol@gmail.com>
2016-12-09 00:27:09 +0000

    Configure cljsbuild auto testing

--- natural-detective/project.clj
+++ natural-detective/project.clj
@@ -11,27 +11,30 @@
             :plugins [[lein-cljsbuild "1.1.4"]
                       [lein-figwheel "0.5.8"]]
             :aliases {"update" ["do" "clean," "deps," "check"]
-                      "cljstest" ["do" "clean," "cljsbuild" "test"]}
+                      "cljstest" ["do" "clean," "cljsbuild" "auto"]}
             :clean-targets ^{:protect false} [".lein-failures"
                                               "figwheel_server.log"
                                               "resources/public/js"
                                               "resources/test/game-test.js"
                                               :target-path]
-            :cljsbuild {:test-commands {"test" ["bin/phantomjs"
-                                                "resources/test/test.js"
-                                                "resources/test/test.html"]}
-                        :builds
+            :cljsbuild {:builds
                         {:dev {:source-paths ["src"]
                                :figwheel true
                                :compiler {:main "game.core"
-                                          :asset-path "js"
                                           :output-to "resources/public/js/game.js"
-                                          :output-dir "resources/public/js"}}
+                                          :output-dir "resources/public/js"
+                                          :asset-path "js"}}
                          :test {:source-paths ["src" "test"]
+                                :notify-command ["bin/phantomjs"
+                                                 "resources/test/test.js"
+                                                 "resources/test/test.html"]
                                 :compiler {:output-to "resources/test/game-test.js"
-                                           :optimizations :whitespace}}}}
+                                           :optimizations :whitespace}}}
+                        :test-commands {"test" ["bin/phantomjs"
+                                                "resources/test/test.js"
+                                                "resources/test/test.html"]}}
             :figwheel {:css-dirs ["resources/public/css"]
                        :builds-to-start [:dev]
-                       :nrepl-port 7888
                        :nrepl-host "localhost"
+                       :nrepl-port 7888
                        :validate-interactive :fix})
