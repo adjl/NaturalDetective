c8f047c5efad4fceb0740bf09ecaf37fc0e04bfe
Adei Josol <helena.josol@gmail.com>
2016-12-11 10:36:16 +0000

    Swap assertion argument order (expected first)

--- natural-detective/test/game/natural_deduction_test.cljs
+++ natural-detective/test/game/natural_deduction_test.cljs
@@ -3,11 +3,11 @@
             [game.natural-deduction :as nd]))
 
 (deftest test-double-negation-introduction
-  (is (= (nd/double-negation-introduction :P) [:not [:not :P]]))
-  (is (= (nd/double-negation-introduction [:not :P])
-         [:not [:not [:not :P]]])))
+  (is (= [:not [:not :P]] (nd/double-negation-introduction :P)))
+  (is (= [:not [:not [:not :P]]]
+         (nd/double-negation-introduction [:not :P]))))
 
 (deftest test-double-negation-elimination
-  (is (= (nd/double-negation-elimination [:not [:not :P]]) :P))
-  (is (= (nd/double-negation-elimination [:not [:not [:not :P]]])
-         [:not :P])))
+  (is (= :P (nd/double-negation-elimination [:not [:not :P]])))
+  (is (= [:not :P]
+         (nd/double-negation-elimination [:not [:not [:not :P]]]))))
