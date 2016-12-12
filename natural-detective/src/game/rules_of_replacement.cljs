4c9f7583c3e15ecae2611e4a6d0f345baa11a652
Adei Josol <helena.josol@gmail.com>
2016-12-12 14:28:59 +0000

    Sanitise comments and docstrings for readability

--- natural-detective/src/game/rules_of_replacement.cljs
+++ natural-detective/src/game/rules_of_replacement.cljs
@@ -35,43 +35,43 @@
       (->> proposition (take-last 2) vec))))
 
 (defn tautology
-  "(P ∧ P) → P
-   (P ∨ P) → P"
+  "P ∧ P → P
+   P ∨ P → P"
   [proposition]
   (match proposition
     [_ P _P]
     (if (= P _P) P)))
 
 (defn commutativity
-  "(P ∧ Q) ↔ (Q ∧ P)
-   (P ∨ Q) ↔ (Q ∨ P)"
+  "P ∧ Q ↔ Q ∧ P
+   P ∨ Q ↔ Q ∨ P"
   [proposition]
   (match proposition
     [c P Q] [c Q P]))
 
 (defn associativity
-  "(P ∧ (Q ∧ R)) ↔ ((P ∧ Q) ∧ R)
-   (P ∨ (Q ∨ R)) ↔ ((P ∨ Q) ∨ R)"
+  "P ∧ (Q ∧ R) ↔ (P ∧ Q) ∧ R
+   P ∨ (Q ∨ R) ↔ (P ∨ Q) ∨ R"
   [proposition]
   (match proposition
-    ;; (P ∘ (Q ∘ R)) → ((P ∘ Q) ∘ R)
+    ;; P∘(Q∘R) → (P∘Q)∘R
     [c P [_c Q R]]
     (if (= c _c)
       [c [c P Q] R])
 
-    ;; ((P ∘ Q) ∘ R) → (P ∘ (Q ∘ R))
+    ;; (P∘Q)∘R → P∘(Q∘R)
     [c [_c P Q] R]
     (if (= c _c)
       [c P [c Q R]])))
 
 (defn distributivity
-  "(P ∧ (Q ∧ R)) ↔ ((P ∧ Q) ∧ (Q ∧ R))
-   (P ∨ (Q ∨ R)) ↔ ((P ∨ Q) ∨ (Q ∨ R))
-   (P ∧ (Q ∨ R)) ↔ ((P ∧ Q) ∨ (Q ∧ R))
-   (P ∨ (Q ∧ R)) ↔ ((P ∨ Q) ∧ (Q ∨ R))"
+  "P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (Q ∧ R)
+   P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (Q ∨ R)
+   (P ∨ Q) ∧ (R ∨ S) ↔ (P ∨ R) ∧ (P ∨ S) ∧ (Q ∨ R) ∧ (Q ∨ S)
+   (P ∧ Q) ∨ (R ∧ S) ↔ (P ∧ R) ∨ (P ∧ S) ∨ (Q ∧ R) ∨ (Q ∧ S)"
   [proposition]
   (match proposition
-    ;; (((P ∘ R) ∘ (P ∘ S)) ∘ ((Q ∘ R) ∘ (Q ∘ S))) → ((P ∘ Q) ∘ (R ∘ S))
+    ;; (P∘R)∘(P∘S)∘(Q∘R)∘(Q∘S) → (P∘Q)∘(R∘S)
     [co
      [_co [ci P R] [_ci _P S]]
      [__co [__ci Q _R] [___ci _Q _S]]]
@@ -79,17 +79,17 @@
              (= P _P) (= Q _Q) (= R _R) (= S _S))
       [ci [co P Q] [co R S]])
 
-    ;; ((P ∘ Q) ∘ (P|R ∘ R|S)) → ...
+    ;; (P∘Q)∘(P|R∘R|S) → ...
     [co [ci P Q] [_ci R S]]
     (if (and (= ci _ci) (= P R))
-      ;; ((P ∘ Q) ∘ (P ∘ R)) → (P ∘ (Q ∘ R))
+      ;; (P∘Q)∘(P∘R) → P∘(Q∘R)
       [ci P [co Q S]]
-      ;; ((P ∘ Q) ∘ (R ∘ S)) → (((P ∘ R) ∘ (P ∘ S)) ∘ ((Q ∘ R) ∘ (Q ∘ S)))
+      ;; (P∘Q)∘(R∘S) → (P∘R)∘(P∘S)∘(Q∘R)∘(Q∘S)
       [ci
        [ci [co P R] [co P S]]
        [ci [co Q R] [co Q S]]])
 
-    ;; (P ∘ (Q ∘ R)) → ((P ∘ Q) ∘ (P ∘ R))
+    ;; P∘(Q∘R) → (P∘Q)∘(P∘R)
     [co P [ci Q R]]
     [ci [co P Q] [co P R]]))
 
