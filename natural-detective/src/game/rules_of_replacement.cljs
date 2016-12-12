534ac51eb91114a842c676401231cb2d481480a9
Adei Josol <helena.josol@gmail.com>
2016-12-12 14:38:02 +0000

    Move references to TODO

--- natural-detective/src/game/rules_of_replacement.cljs
+++ natural-detective/src/game/rules_of_replacement.cljs
@@ -92,18 +92,3 @@
     ;; P∘(Q∘R) → (P∘Q)∘(P∘R)
     [co P [ci Q R]]
     [ci [co P Q] [co P R]]))
-
-;; References
-;; https://en.wikipedia.org/wiki/Rule_of_replacement, 11/12/16
-;; https://en.wikipedia.org/wiki/Double_negation, 11/12/16
-;; https://en.wikipedia.org/wiki/Tautology_(rule_of_inference), 11/12/16
-;; https://en.wikipedia.org/wiki/Commutative_property, 11/12/16
-;; https://en.wikipedia.org/wiki/Associative_property, 11/12/16
-;; https://en.wikipedia.org/wiki/Distributive_property, 11/12/16
-
-;; https://en.wikipedia.org/wiki/Rule_of_inference, 11/12/16
-;; https://en.wikipedia.org/wiki/Premise, 11/12/16
-
-;; https://en.wikipedia.org/wiki/List_of_mathematical_symbols, 11/12/16
-;; https://en.wikipedia.org/wiki/List_of_logic_symbols, 11/12/16
-;; https://en.wikipedia.org/wiki/Proposition, 11/12/16
