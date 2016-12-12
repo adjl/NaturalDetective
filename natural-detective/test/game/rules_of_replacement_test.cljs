c90f57e7255be271a6deac125eb721a64cfc86bc
Adei Josol <helena.josol@gmail.com>
2016-12-12 13:26:20 +0000

    Use cljs.test/testing macro to document tests

--- natural-detective/test/game/rules_of_replacement_test.cljs
+++ natural-detective/test/game/rules_of_replacement_test.cljs
@@ -1,75 +1,91 @@
 (ns game.rules-of-replacement-test
-  (:require [cljs.test :refer-macros [deftest is]]
+  (:require [cljs.test :refer-macros [deftest is testing]]
             [game.rules-of-replacement :as r]))
 
 (deftest test-double-negation-introduction
-  (is (= [:not [:not :P]] (r/double-negation-introduction :P)))
-  (is (= [:not [:not [:not :P]]] (r/double-negation-introduction [:not :P]))))
+  (testing "Double negation introduction"
+    (is (= [:not [:not :P]] (r/double-negation-introduction :P)))
+    (is (= [:not [:not [:not :P]]] (r/double-negation-introduction [:not :P])))))
 
 (deftest test-double-negation-elimination
-  (is (= :P (r/double-negation-elimination [:not [:not :P]])))
-  (is (= [:not :P] (r/double-negation-elimination [:not [:not [:not :P]]]))))
+  (testing "Double negation elimination"
+    (is (= :P (r/double-negation-elimination [:not [:not :P]])))
+    (is (= [:not :P] (r/double-negation-elimination [:not [:not [:not :P]]])))))
 
 (deftest test-tautology
-  (is (= :P (r/tautology [:and :P :P])))
-  (is (= :P (r/tautology [:or :P :P]))))
+  (testing "Idempotency of conjunction"
+    (is (= :P (r/tautology [:and :P :P]))))
+  (testing "Idempotency of disjunction"
+    (is (= :P (r/tautology [:or :P :P])))))
 
 (deftest test-commutativity
-  (is (= [:and :Q :P] (r/commutativity [:and :P :Q])))
-  (is (= [:or :Q :P] (r/commutativity [:or :P :Q]))))
+  (testing "Commutativity of conjunction"
+    (is (= [:and :Q :P] (r/commutativity [:and :P :Q]))))
+  (testing "Commutativity of disjunction"
+    (is (= [:or :Q :P] (r/commutativity [:or :P :Q])))))
 
 (deftest test-associativity
-  (is (= [:and [:and :P :Q] :R] (r/associativity [:and :P [:and :Q :R]])))
-  (is (= [:or [:or :P :Q] :R] (r/associativity [:or :P [:or :Q :R]])))
-  (is (= [:and :P [:and :Q :R]] (r/associativity [:and [:and :P :Q] :R])))
-  (is (= [:or :P [:or :Q :R]] (r/associativity [:or [:or :P :Q] :R]))))
+  (testing "Associativity of conjunction"
+    (is (= [:and [:and :P :Q] :R] (r/associativity [:and :P [:and :Q :R]])))
+    (is (= [:and :P [:and :Q :R]] (r/associativity [:and [:and :P :Q] :R]))))
+  (testing "Associativity of disjunction"
+    (is (= [:or [:or :P :Q] :R] (r/associativity [:or :P [:or :Q :R]])))
+    (is (= [:or :P [:or :Q :R]] (r/associativity [:or [:or :P :Q] :R])))))
 
 (deftest test-distributivity
-  (is (= [:and [:and :P :Q] [:and :P :R]]
-         (r/distributivity [:and :P [:and :Q :R]])))
-  (is (= [:or [:or :P :Q] [:or :P :R]]
-         (r/distributivity [:or :P [:or :Q :R]])))
-  (is (= [:or [:and :P :Q] [:and :P :R]]
-         (r/distributivity [:and :P [:or :Q :R]])))
-  (is (= [:and [:or :P :Q] [:or :P :R]]
-         (r/distributivity [:or :P [:and :Q :R]])))
-  (is (= [:and :P [:and :Q :R]]
-         (r/distributivity [:and [:and :P :Q] [:and :P :R]])))
-  (is (= [:or :P [:or :Q :R]]
-         (r/distributivity [:or [:or :P :Q] [:or :P :R]])))
-  (is (= [:or :P [:and :Q :R]]
-         (r/distributivity [:and [:or :P :Q] [:or :P :R]])))
-  (is (= [:and :P [:or :Q :R]]
-         (r/distributivity [:or [:and :P :Q] [:and :P :R]])))
-  (is (= [:and
-          [:and [:and :P :R] [:and :P :S]]
-          [:and [:and :Q :R] [:and :Q :S]]]
-         (r/distributivity [:and [:and :P :Q] [:and :R :S]])))
-  (is (= [:or
-          [:or [:or :P :R] [:or :P :S]]
-          [:or [:or :Q :R] [:or :Q :S]]]
-         (r/distributivity [:or [:or :P :Q] [:or :R :S]])))
-  (is (= [:or
-          [:or [:and :P :R] [:and :P :S]]
-          [:or [:and :Q :R] [:and :Q :S]]]
-         (r/distributivity [:and [:or :P :Q] [:or :R :S]])))
-  (is (= [:and
-          [:and [:or :P :R] [:or :P :S]]
-          [:and [:or :Q :R] [:or :Q :S]]]
-         (r/distributivity [:or [:and :P :Q] [:and :R :S]])))
-  (is (= [:and [:and :P :Q] [:and :R :S]]
-         (r/distributivity [:and
-                            [:and [:and :P :R] [:and :P :S]]
-                            [:and [:and :Q :R] [:and :Q :S]]])))
-  (is (= [:or [:or :P :Q] [:or :R :S]]
-         (r/distributivity [:or
-                            [:or [:or :P :R] [:or :P :S]]
-                            [:or [:or :Q :R] [:or :Q :S]]])))
-  (is (= [:or [:and :P :Q] [:and :R :S]]
-         (r/distributivity [:and
-                            [:and [:or :P :R] [:or :P :S]]
-                            [:and [:or :Q :R] [:or :Q :S]]])))
-  (is (= [:and [:or :P :Q] [:or :R :S]]
-         (r/distributivity [:or
-                            [:or [:and :P :R] [:and :P :S]]
-                            [:or [:and :Q :R] [:and :Q :S]]]))))
+  (testing "Distribution of conjunction over conjunction"
+    (is (= [:and [:and :P :Q] [:and :P :R]]
+           (r/distributivity [:and :P [:and :Q :R]])))
+    (is (= [:and :P [:and :Q :R]]
+           (r/distributivity [:and [:and :P :Q] [:and :P :R]]))))
+  (testing "Distribution of disjunction over disjunction"
+    (is (= [:or [:or :P :Q] [:or :P :R]]
+           (r/distributivity [:or :P [:or :Q :R]])))
+    (is (= [:or :P [:or :Q :R]]
+           (r/distributivity [:or [:or :P :Q] [:or :P :R]]))))
+  (testing "Distribution of conjunction over disjunction"
+    (is (= [:or [:and :P :Q] [:and :P :R]]
+           (r/distributivity [:and :P [:or :Q :R]])))
+    (is (= [:or :P [:and :Q :R]]
+           (r/distributivity [:and [:or :P :Q] [:or :P :R]]))))
+  (testing "Distribution of disjunction over conjunction"
+    (is (= [:and [:or :P :Q] [:or :P :R]]
+           (r/distributivity [:or :P [:and :Q :R]])))
+    (is (= [:and :P [:or :Q :R]]
+           (r/distributivity [:or [:and :P :Q] [:and :P :R]]))))
+  (testing "Double distribution of conjunction over conjunction"
+    (is (= [:and
+            [:and [:and :P :R] [:and :P :S]]
+            [:and [:and :Q :R] [:and :Q :S]]]
+           (r/distributivity [:and [:and :P :Q] [:and :R :S]])))
+    (is (= [:and [:and :P :Q] [:and :R :S]]
+           (r/distributivity [:and
+                              [:and [:and :P :R] [:and :P :S]]
+                              [:and [:and :Q :R] [:and :Q :S]]]))))
+  (testing "Double distribution of disjunction over disjunction"
+    (is (= [:or
+            [:or [:or :P :R] [:or :P :S]]
+            [:or [:or :Q :R] [:or :Q :S]]]
+           (r/distributivity [:or [:or :P :Q] [:or :R :S]])))
+    (is (= [:or [:or :P :Q] [:or :R :S]]
+           (r/distributivity [:or
+                              [:or [:or :P :R] [:or :P :S]]
+                              [:or [:or :Q :R] [:or :Q :S]]]))))
+  (testing "Double distribution of conjunction over disjunction"
+    (is (= [:or
+            [:or [:and :P :R] [:and :P :S]]
+            [:or [:and :Q :R] [:and :Q :S]]]
+           (r/distributivity [:and [:or :P :Q] [:or :R :S]])))
+    (is (= [:or [:and :P :Q] [:and :R :S]]
+           (r/distributivity [:and
+                              [:and [:or :P :R] [:or :P :S]]
+                              [:and [:or :Q :R] [:or :Q :S]]]))))
+  (testing "Double distribution of disjunction over conjunction"
+    (is (= [:and
+            [:and [:or :P :R] [:or :P :S]]
+            [:and [:or :Q :R] [:or :Q :S]]]
+           (r/distributivity [:or [:and :P :Q] [:and :R :S]])))
+    (is (= [:and [:or :P :Q] [:or :R :S]]
+           (r/distributivity [:or
+                              [:or [:and :P :R] [:and :P :S]]
+                              [:or [:and :Q :R] [:and :Q :S]]])))))
