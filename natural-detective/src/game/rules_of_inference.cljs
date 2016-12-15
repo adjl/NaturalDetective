fd552e649d000a6f4c6416146e0e1afb1fd4a4e5
Adei Josol <helena.josol@gmail.com>
2016-12-15 17:17:45 +0000

    Use `metavar-*` as wildcards

--- natural-detective/src/game/rules_of_inference.cljs
+++ natural-detective/src/game/rules_of_inference.cljs
@@ -11,12 +11,12 @@
    (disjunctive-syllogism [premise-0 premise-1]))
   ([premises]
    (match premises
-     [[:or P Q] [:not _P]]
-     (cond (= P _P) Q     ;; P ∨ Q, ¬P
-           (= Q _P) P)    ;; Q ∨ P, ¬P
-     [[:not _P] [:or P Q]]
-     (cond (= P _P) Q     ;; ¬P, P ∨ Q
-           (= Q _P) P)))) ;; ¬P, Q ∨ P
+     [[:or metavar-0 metavar-1] [:not metavar-2]]
+     (cond (= metavar-0 metavar-2) metavar-1     ;; P ∨ Q, ¬P
+           (= metavar-1 metavar-2) metavar-0)    ;; Q ∨ P, ¬P
+     [[:not metavar-2] [:or metavar-0 metavar-1]]
+     (cond (= metavar-0 metavar-2) metavar-1     ;; ¬P, P ∨ Q
+           (= metavar-1 metavar-2) metavar-0)))) ;; ¬P, Q ∨ P
 
 (defn hypothetical-syllogism
   "P → Q, Q → R ⊢ P → R"
@@ -24,6 +24,8 @@
    (hypothetical-syllogism [premise-0 premise-1]))
   ([premises]
    (match premises
-     [[:impl P Q] [:impl _Q R]]
-     (cond (= Q _Q) [:impl P R]      ;; P → Q, Q → R
-           (= P R)  [:impl _Q Q])))) ;; Q → R, P → Q
+     [[:impl metavar-0 metavar-1] [:impl metavar-2 metavar-3]]
+     (cond (= metavar-1 metavar-2) ;; P → Q, Q → R
+           [:impl metavar-0 metavar-3]
+           (= metavar-0 metavar-3) ;; Q → R, P → Q
+           [:impl metavar-2 metavar-1]))))
