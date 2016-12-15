0af4da548193086feb5cd8337b99ee2365e00001
Adei Josol <helena.josol@gmail.com>
2016-12-15 16:58:56 +0000

    Disjunctive syllogism

--- natural-detective/src/game/rules_of_inference.cljs
+++ natural-detective/src/game/rules_of_inference.cljs
@@ -2,3 +2,16 @@
   (:require [cljs.core.match :refer-macros [match]]))
 
 ;; Rules of Inference (Rules of Implication)
+;; - Disjunctive syllogism (Modus tollendo ponens)
+(defn disjunctive-syllogism
+  "P ∨ Q, ¬P ⊢ Q"
+  ([premise-0 premise-1]
+   (disjunctive-syllogism [premise-0 premise-1]))
+  ([premises]
+   (match premises
+     [[:or P Q] [:not _P]]
+     (cond (= P _P) Q     ;; P ∨ Q, ¬P
+           (= Q _P) P)    ;; Q ∨ P, ¬P
+     [[:not _P] [:or P Q]]
+     (cond (= P _P) Q     ;; ¬P, P ∨ Q
+           (= Q _P) P)))) ;; ¬P, Q ∨ P
--- natural-detective/test/game/rules_of_inference_test.cljs
+++ natural-detective/test/game/rules_of_inference_test.cljs
@@ -1,3 +1,10 @@
 (ns game.rules-of-inference-test
   (:require [cljs.test :refer-macros [deftest is testing]]
             [game.rules-of-inference :as i]))
+
+(deftest test-disjunctive-syllogism
+  (testing "Disjunctive syllogism"
+    (is (= :Q (i/disjunctive-syllogism [:or :P :Q] [:not :P])))
+    (is (= :Q (i/disjunctive-syllogism [:or :Q :P] [:not :P])))
+    (is (= :Q (i/disjunctive-syllogism [:not :P] [:or :P :Q])))
+    (is (= :Q (i/disjunctive-syllogism [:not :P] [:or :Q :P])))))
