015766c4650cdfb90850bd12437b19f9b823805f
Adei Josol <helena.josol@gmail.com>
2016-12-15 14:46:53 +0000

    Create source and test files for rules of inference

---
+++ natural-detective/src/game/rules_of_inference.cljs
@@ -0,0 +1,4 @@
+(ns game.rules-of-inference
+  (:require [cljs.core.match :refer-macros [match]]))
+
+;; Rules of Inference (Rules of Implication)
---
+++ natural-detective/test/game/rules_of_inference_test.cljs
@@ -0,0 +1,3 @@
+(ns game.rules-of-inference-test
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [game.rules-of-inference :as i]))
